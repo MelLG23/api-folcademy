2b28094da73067f7363778378ddf7d0b7e3a8326
MelaniaLG <melania_maidana@hotmail.com>
2023-10-13 04:53:23 -0300

    commit desafio 3

---
+++ src/main/java/com/folcademy/exampleapi/Models/Dtos/UserEditDTO.java
@@ -0,0 +1,13 @@
+package com.folcademy.exampleapi.Models.Dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEditDTO {
+        String name;
+        String surname;
+}
